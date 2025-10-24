26214405ff22c657ba7fc593fb3e29cc6cf5a41e
Ak0sTib0r <102594815+Ak0sTib0r@users.noreply.github.com>
2025-10-24 14:12:19 +0100

    Add files via upload

--- UnderGrad/main.py
+++ UnderGrad/main.py
@@ -8,7 +8,7 @@
  """
 
 __author__ = "Ákos Hadar"
-__version__ = "0.5"
+__version__ = "1.0"
 
 import customtkinter
 import os
--- UnderGrad/mktemp.py
+++ UnderGrad/mktemp.py
@@ -10,16 +10,31 @@ homePath = configList["home_path"]
 instOpen = configList["inst_open"]
 
 def CreateDocument(title):
-    if title != "":
+    if genType == "boxed":
+        if title != "":
+            os.chdir(genPath)
+            os.mkdir(f'{title}')
+
+            source = f'{homePath}\\temp.tex'
+            dest = f'{genPath}\\{title}\\{title}.tex'
+
+            shutil.copyfile(source, dest)
+
+            os.chdir(f'{genPath}\\{title}')
+
+            if instOpen == "true":
+                os.system(f"{title}.tex")
+        else:
+            pass
+    elif genType == "free":
         os.chdir(genPath)
-        os.mkdir(f'{title}')
 
         source = f'{homePath}\\temp.tex'
-        dest = f'{genPath}\\{title}\\{title}.tex'
+        dest = f'{genPath}\\{title}.tex'
 
         shutil.copyfile(source, dest)
 
-        os.chdir(f'{genPath}\\{title}')
-        os.system(f"{title}.tex")
-    else:
-        pass
+        os.chdir(f'{genPath}')
+        
+        if instOpen == "true":
+            os.system(f"{title}.tex")
